de1c130bdac9feaf9f4794768ebcb3337dee487a
VajihuddinAhmed <vajihahmed@gmail.com>
2021-01-24 16:13:45 +0100

    snapshots files added and other issues solved

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
-import './styles/styles.scss';
+import './styles/style.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase'
 
