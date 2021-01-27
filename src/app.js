a97eb67070f45d68af73ed49c5d4f32e2a5b21b4
VajihuddinAhmed <vajihahmed@gmail.com>
2021-01-27 16:32:12 +0100

    redirecting login and logout code complete

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
+import { login, logout } from './actions/auth';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
@@ -19,16 +19,28 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(<p>Load...</p>, document.getElementById('app'));
+let hasRendered = false
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(jsx, document.getElementById('app'))
+    hasRendered = true
+  }
+}
 
-store.dispatch(startSetExpenses()).then(() => {
-  ReactDOM.render(jsx, document.getElementById('app'));
-})
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
-    console.log('log in')
+    store.dispatch(login(user.uid))
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp()
+      if (history.location.pathname === '/') {
+        history.push('/dashboard')
+      }
+    })
   } else {
-    console.log('log out')
+    store.dispatch(logout())
+    renderApp()
+    history.push('/')
   }
 })
