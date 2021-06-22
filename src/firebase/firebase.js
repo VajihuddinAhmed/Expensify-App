65863d28a15d93669c0858b9234799d60aefc000
VajihuddinAhmed <vajihahmed@gmail.com>
2021-06-22 22:33:09 +0200

    changes were made in firebase authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 import 'firebase/database';
 
 const config = {
-    apiKey: "AIzaSyBc0LzZiGRB_hBLUSUx9viR_CLAdLKt1Yw",
-    authDomain: "expensify-c4de7.firebaseapp.com",
-    databaseURL: "https://expensify-c4de7-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "expensify-c4de7",
-    storageBucket: "expensify-c4de7.appspot.com",
-    messagingSenderId: "844032377565",
-    appId: "1:844032377565:web:ff408202b9016636add134",
-    measurementId: "G-ELZMWNX97J"
+    apiKey: "AIzaSyDEsg7n0Q7kILP8Xfzh5o9h2iZnjuojSKs",
+    authDomain: "expensify-app-31186.firebaseapp.com",
+    databaseURL: "https://expensify-app-31186-default-rtdb.firebaseio.com",
+    projectId: "expensify-app-31186",
+    storageBucket: "expensify-app-31186.appspot.com",
+    messagingSenderId: "240307474526",
+    appId: "1:240307474526:web:425279ede6919e1c0564cf",
+    measurementId: "G-P80LCE5YDD"
 };
 
 firebase.initializeApp(config)
